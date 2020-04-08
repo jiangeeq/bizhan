d02f44bc13c7ba8f89525eff0ee8c4af97c69e1c
蒋鹏 <773899172@qq.com>
2020-04-08 23:37:21 +0800

    多种测试方式

--- 手写简易版mybatis/mybatis-demo/src/main/java/com/jls/mybatis/test/MybatisTest.java
+++ 手写简易版mybatis/mybatis-demo/src/main/java/com/jls/mybatis/test/MybatisTest.java
@@ -1,43 +1,42 @@
-//package com.kkb.mybatis.test;
-//
-//import java.io.InputStream;
-//
-//import org.junit.Test;
-//
-//import com.kkb.mybatis.framework.builder.Configuration;
-//import com.kkb.mybatis.framework.builder.SqlSessionFactoryBuilder;
-//import com.kkb.mybatis.framework.builder.XmlConfigBuilder;
-//import com.kkb.mybatis.framework.resource.Resources;
-//import com.kkb.mybatis.framework.sqlsession.SqlSession;
-//import com.kkb.mybatis.framework.sqlsession.SqlSessionFactory;
-//import com.kkb.mybatis.po.User;
-//
-//public class MybatisTest {
-//
-//	@Test
-//	public void test() throws Exception {
-//		// 指定全局配置文件的类路径
-//		String resource = "SqlMapConfig.xml";
-//		InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(resource);
-//		// InputStream inputStream = Resources.getResourceAsStream(resource);
-//		
-//		XmlConfigBuilder builder = new XmlConfigBuilder(inputStream);
-//		Configuration configuration = builder.parse();
-//		System.out.println(configuration);
-//	}
-//
-//	@Test
-//	public void test1() throws Exception {
-//		// 指定全局配置文件的类路径
-//		String resource = "SqlMapConfig.xml";
-//		InputStream inputStream = Resources.getResourceAsStream(resource);
-//
-//		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-//
-//		SqlSession sqlSession = sqlSessionFactory.openSqlSession();
-//
-//		User user = sqlSession.selectOne("findUserById", 1);
-//
-//		System.out.println(user);
-//	}
-//}
+package com.jls.mybatis.test;
+
+import java.io.InputStream;
+
+import com.jls.mybatis.po.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.builder.xml.XMLConfigBuilder;
+import org.junit.Test;
+
+public class MybatisTest {
+
+	@Test
+	public void test() throws Exception {
+		// 指定全局配置文件的类路径
+		String resource = "SqlMapConfig2.xml";
+		InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(resource);
+		// InputStream inputStream = Resources.getResourceAsStream(resource);
+
+		XMLConfigBuilder builder = new XMLConfigBuilder(inputStream);
+		Configuration configuration = builder.parse();
+		System.out.println(configuration);
+	}
+
+	@Test
+	public void test1() throws Exception {
+		// 指定全局配置文件的类路径
+		String resource = "SqlMapConfig2.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+
+		User user = sqlSession.selectOne("findUserById", 1);
+
+		System.out.println(user);
+	}
+}
